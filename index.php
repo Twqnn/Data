468e77db8976d8bc37df17ee274515c0f52e3e87
Twqnn <135101612+Twqnn@users.noreply.github.com>
2023-11-24 13:40:12 +0100

    Add files via upload
    opdr2

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "Winkel"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed " . $conn->connect_error);
+}
+echo "Connected to database ($dbname).";
+
+?>
+
+
+</body>
+</html>
